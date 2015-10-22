297ca552e43cb82b96daca499fce131d222f0e27
Jonathan Hood <me@jonathanhood.org>
2015-10-21 22:22:57 -0500

    Add tests for RandomStrategy

---
+++ src/test/scala/TestRandomStrategy.scala
@@ -0,0 +1,43 @@
+import org.scalatest.{Matchers, WordSpec}
+import com.jhood.battlebot._
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class TestRandomStrategy extends WordSpec with Matchers {
+  val strategy = new RandomStrategy()
+
+  def assertRequestResponse(request: GameMessage, response: ResponseMessage) = {
+    val result = Await.result(strategy.update(request), 1 second)
+    result should be (Some(response))
+  }
+
+  def assertRequestNoResponse(request: GameMessage) = {
+    val result = Await.result(strategy.update(request), 1 second)
+    result should be (None)
+  }
+
+  "A RandomStrategy" should {
+    "respond with a name" in {
+      assertRequestResponse(
+        NameRequest(North),
+        NameSet(North, "RandomStrategy")
+      )
+    }
+
+    "respond with a dummy play" in {
+      assertRequestResponse(
+        PlayCard(),
+        PlayCardResponse(1, Card("color1",1))
+      )
+    }
+
+    "not respond to all other messages" in {
+      assertRequestNoResponse(OpponentPlay(1, Card("color1",1)))
+      assertRequestNoResponse(FlagCards(1, North, List()))
+      assertRequestNoResponse(FlagClaimStatus(List()))
+      assertRequestNoResponse(PlayerHand(North,List()))
+      assertRequestNoResponse(ColorsDeclaration(List()))
+    }
+  }
+
+}
