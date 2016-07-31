9701f5b86f5daa2bd5244c9fa93517202a52bdda
Jonathan Hood <me@jonathanhood.org>
2016-07-31 15:45:37 -0500

    First steps towards a new strategy

---
+++ src/main/scala/com/jhood/battlebot/BreakthroughStrategy.scala
@@ -0,0 +1,42 @@
+
+package com.jhood.battlebot
+
+import scala.concurrent.Future
+import scala.collection.mutable.Map
+
+class BreakthroughStrategy extends Strategy {
+  var myDirection: Direction = North
+
+  var myFlags: Map[Int,List[Card]] = Map(
+    1 -> List(), 2 -> List(), 3 -> List(), 4 -> List(),
+    5 -> List(), 6 -> List(), 7 -> List(), 8 -> List(),
+    9 -> List()
+  )
+
+  var opponentFlags: Map[Int,List[Card]] = Map(
+    1 -> List(), 2 -> List(), 3 -> List(), 4 -> List(),
+    5 -> List(), 6 -> List(), 7 -> List(), 8 -> List(),
+    9 -> List()
+  )
+
+  var hand: List[Card] = List()
+ 
+  def update(msg: GameMessage): Future[Option[ResponseMessage]] = Future.successful(
+    msg match {
+      case NameRequest(direction) => 
+        myDirection = direction
+        Some(NameSet(direction, "darthbagel"))
+      case FlagCards(flag,direction,cards) =>
+        if(direction == myDirection) myFlags += ((flag,cards))
+        else opponentFlags += ((flag,cards))
+        None
+      case PlayerHand(dir,cards) => 
+        hand = cards
+        None
+      case PlayCard() => 
+        Some(PlayCardResponse(1, Card("color1",1)))
+      case _ => 
+        None
+    }
+  )
+}
