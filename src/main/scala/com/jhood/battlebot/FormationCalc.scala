ddcfcb02efffc021f4c6a7cccc04fdf79deb821f
Jonathan Hood <me@jonathanhood.org>
2016-07-31 15:20:08 -0500

    Finish up formation scoring

--- src/main/scala/com/jhood/battlebot/FormationCalc.scala
+++ src/main/scala/com/jhood/battlebot/FormationCalc.scala
@@ -3,16 +3,45 @@ package com.jhood.battlebot
 object FormationCalc {
 
   def score(cards: List[Card]): Int =
-    battallion(cards).getOrElse(0)
+    wedge(cards)
+      .orElse(phallanx(cards))
+      .orElse(battalion(cards))
+      .orElse(skirmish(cards))
+      .getOrElse(sum(cards))
 
-  def battallion(cards: List[Card]): Option[Int] =
+  def wedge(cards: List[Card]): Option[Int] = 
+    for(
+      skirmishValue <- skirmish(cards);
+      battalionValue <- battalion(cards)
+    ) yield sum(cards) + 500
+
+  def phallanx(cards: List[Card]): Option[Int] =
+    if(cards.length == 3 && cards.forall(_.value == cards.head.value))
+      Some(sum(cards) + 400)
+    else
+      None 
+
+  def battalion(cards: List[Card]): Option[Int] =
     cards match {
       case List(first,second,third) =>
         if(first.suite == second.suite && first.suite == third.suite)
-          Some(cards.map(_.value).reduce(_ + _) + 300)
+          Some(sum(cards) + 300)
         else 
           None
       case _ => None
     }
+
+  def skirmish(cards: List[Card]): Option[Int] =
+    if(cards.length == 3) {
+      val sortedCards = cards.map(_.value).sorted
+      val expected = (sortedCards.head to (sortedCards.head + cards.size))
+      if(sortedCards.zip(expected).forall(x => x._1 == x._2)) Some(sum(cards) + 200)
+      else None
+    } else {
+      None
+    }
+
+  def sum(cards: List[Card]): Int =
+    cards.map(_.value).fold(0)(_ + _)
 }
 
--- src/test/scala/TestFormationCalc.scala
+++ src/test/scala/TestFormationCalc.scala
@@ -3,15 +3,34 @@ import com.jhood.battlebot._
 
 class TestFormationCalc extends WordSpec with Matchers {
   "A FormationCalc" should {
-    "detect a battalion and return a 300 score with valueless cardss" in {
-      val cards = List(Card("g", 0), Card("g", 0), Card("g", 0 ))
-      FormationCalc.score(cards) should be (300)
-    }    
+    "detect a wedge and return a 5xx score" in {
+      val cards = List(Card("a", 1), Card("a", 2), Card("a", 3))
+      FormationCalc.score(cards) should be (506)
+    }
+
+    "detect a phallanx and return a 4xx score" in {
+      val cards = List(Card("a", 1), Card("b",1), Card("c",1))
+      FormationCalc.score(cards) should be (403)
+    }
     
     "detect a battalion and return a 3xx score with valued cards" in {
-      val cards = List(Card("g", 1), Card("g", 2), Card("g", 3))
-      FormationCalc.score(cards) should be (306)
-    }    
+      val cards = List(Card("g", 1), Card("g", 8), Card("g", 5))
+      FormationCalc.score(cards) should be (314)
+    }
+
+    "detect a skirmish and return a 2xx score" in {
+      val cards = List(Card("a", 1), Card("b",2), Card("c",3))
+      FormationCalc.score(cards) should be (206)
+    }
+
+    "return the sum of cards when less than 3 are provided" in {
+      val cards = List(Card("a",1), Card("b",1))
+      FormationCalc.score(cards) should be (2)
+    }
+
+    "return 0 for an empty list" in {
+      FormationCalc.score(List()) should be (0)
+    }
   }
 }
 
