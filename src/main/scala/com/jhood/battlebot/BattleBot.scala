97fdfcf743c03d403d6f45a951c8f3072b7336c3
Jonathan Hood <me@jonathanhood.org>
2016-08-05 21:42:52 -0500

    Add ability to choose strategy at command line

--- src/main/scala/com/jhood/battlebot/BattleBot.scala
+++ src/main/scala/com/jhood/battlebot/BattleBot.scala
@@ -3,13 +3,24 @@ package com.jhood.battlebot
 import scala.io.StdIn.readLine
 
 object BattleBot {
-  val strategy = new HighestScoreStrategy()
-  val wrappedStrategy = new StdMsgStrategyWrapper(strategy)
-
   def main(args: Array[String]): Unit = {
+    val wrappedStrategy = new StdMsgStrategyWrapper(strategy(args))
+
     while(true) {
       val response = wrappedStrategy.update(readLine())
       response.map{ println(_) }
     }
+  }
+
+  def strategy(args: Array[String]): Strategy = args match {
+    case Array("dumb") =>
+      System.err.println("Chose dumb strategy") 
+      new RandomStrategy
+    case Array("highest") => 
+      System.err.println("Chose highest score strategy")
+      new HighestScoreStrategy
+    case _ => 
+      System.err.println("Chose default strategy")
+      new HighestScoreStrategy
   }
 }
